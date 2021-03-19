c1ca7ef35bea182458da40261fdd4c5f8e160158
Branches7 <50388052+Branches7@users.noreply.github.com>
2021-03-19 18:32:50 -0300

    Update Main.java

--- Lista1/CalculadoraAritmetica/Main.java
+++ Lista1/CalculadoraAritmetica/Main.java
@@ -4,9 +4,9 @@ class Main {
   public static void main(String[] args) {
     Scanner num = new Scanner(System.in);
     System.out.println("1 - Multiplicação\n2 - Divisão\n3 - Adição\n4 - Subtração\n");
-    int escolha = num.nextInt();
+    int escolha = num.nextInt(); 
     int n1, n2;
-    switch(escolha){
+    switch(escolha){ //swich para receber a operação desejada
       case 1:
       System.out.println("N1:");
       n1 = num.nextInt();
@@ -19,7 +19,7 @@ class Main {
       n1 = num.nextInt();
       System.out.println("N2:");
       n2 = num.nextInt();
-      try{
+      try{ // Erro ao tentar dividir por zero
         System.out.println(n1 / n2);
       }
       catch(ArithmeticException e){
